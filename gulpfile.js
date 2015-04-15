a1c21786786c00c671c719b0fea4047c74c74197
mouse <mouse@sociomantic.com>
2015-04-15 15:04:52 +0200

    gulp changes to make microbe.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
 var gulp            = require('gulp');
-var fs              = require( 'fs' );
+var fs              = require('fs');
 var clean           = require('gulp-rimraf');
-var concat          = require('gulp-concat');
 var replace         = require('gulp-replace');
+var uglify          = require('gulp-uglify');
+var rename          = require('gulp-rename');
 
 var browserify      = require('browserify');
 var token           = 'ThisIsVeryUnlikelyThatAVariableWillBeCalledThisWay';
 var exportName      = 'µ';
 
 // Basic usage
+gulp.task('dist', function()
+{
+    browserify('./src/microbe.js', {standalone: token})
+        .bundle()
+        .pipe(fs.createWriteStream(__dirname + '/dist/microbe.js'))
+        .on( 'finish', function()
+        {
+            gulp.src('./dist/microbe.js')
+                .pipe(replace(token, exportName))
+                .pipe(gulp.dest('./dist/'))
+                .pipe(uglify())
+                .pipe(rename('./dist/microbe.min.js'))
+                .pipe(gulp.dest('./'));
+        });
+});
+
+
 gulp.task('build', function()
 {
     browserify('./src/microbe.js', {standalone: token})
@@ -22,20 +40,39 @@ gulp.task('build', function()
         });
 });
 
+
+gulp.task('min', function()
+{
+    browserify('./src/microbe.js', {standalone: token})
+        .bundle()
+        .pipe(fs.createWriteStream(__dirname + '/dist/microbe.min.js'))
+        .on( 'finish', function()
+        {
+            gulp.src('./dist/microbe.min.js')
+                .pipe(replace(token, exportName))
+                .pipe(uglify())
+                .pipe(gulp.dest('./dist/'));
+        });
+});
+
+
 gulp.task('test', function()
 {
     return true;
 });
+
 
 gulp.task('clean', function()
 {
     return gulp.src(['dist/'], {read: false}).pipe(clean());
 });
 
+
 gulp.task('default', [], function()
 {
-    gulp.start('build');
+    gulp.start('dist');
 });
+
 
 gulp.task('watch', function()
 {
