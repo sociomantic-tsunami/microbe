f561496182d533866f1e17f63473c8952982f497
mouse <mouse@sociomantic.com>
2015-06-05 14:23:30 +0200

    added to root.js: once insertStyle removeStyle removeStyles
    no tests or docs yet

--- dist/microbe.js
+++ dist/microbe.js
@@ -4335,6 +4335,79 @@ module.exports = function( Microbe )
 
 
     /**
+     * builds a style tag for the given selector/ media query.  Reference to the style
+     * tag and object is saved in µ.__customCSSRules[ selector ][ media ].
+     * next rule with the same selector combines the old and new rules and overwrites
+     * the contents
+     *
+     * @param {String}              selector            selector to apply it to
+     * @param {String or Object}    cssObj              css object.
+     * @param {String}              media               media query
+     *
+     * @return {Microbe or Style }
+     */
+    Microbe.insertStyle = function( selector, cssObj, media )
+    {
+        var _s      = selector.replace( / /g, '-' );
+        var _clss   = media ? _s +  media.replace( /[\s:\/\[\]\(\)]+/g, '-' ) : _s;
+
+        media       = media || 'none';
+
+        var createStyleTag = function()
+        {
+            var el = document.createElement( 'style' );
+            el.className = 'microbe--inserted--style__' + _clss;
+
+            if ( media && media !== 'none' )
+            {
+                el.setAttribute( 'media', media );
+            }
+
+            document.head.appendChild( el );
+
+            return el;
+        };
+
+        var _el, prop;
+        var styleObj =  Microbe.__customCSSRules[ _s ];
+
+        if ( styleObj && styleObj[ media ] )
+        {
+            _el     = styleObj[ media ].el;
+            var obj = styleObj[ media ].obj;
+
+            for ( prop in cssObj )
+            {
+                obj[ prop ] = cssObj[ prop ];
+            }
+
+            cssObj = obj;
+        }
+        else
+        {
+            _el = createStyleTag();
+        }
+
+        var css = selector + '{';
+        for ( prop in cssObj )
+        {
+            css += prop + ' : ' + cssObj[ prop ] + ';';
+        }
+        css += '}';
+
+        _el.innerHTML = css;
+
+        Microbe.__customCSSRules[ _s ] = Microbe.__customCSSRules[ _s ] || {};
+        Microbe.__customCSSRules[ _s ][ media ] = { el: _el, obj: cssObj };
+
+        return _el;
+    };
+
+    // keep track of tags created with insertStyle
+    Microbe.__customCSSRules = {};
+
+
+    /**
      * native isArray for completeness
      *
      * @type {Function}
@@ -4436,6 +4509,92 @@ module.exports = function( Microbe )
 
 
     /**
+     * removes a style tag for the given selector/ media query. If the properties
+     * array is passed, rules are removed individually.  If properties is set to
+     * true, all tags for this selector are removed.  The media query can also be
+     * passed as the second variable
+     *
+     * @param {String}              selector            selector to apply it to
+     * @param {String Array Bool}   properties          css properties to remove
+     *                                                  true to remove all selector tags
+     *                                                  string as media query
+     * @param {String}              media               media query
+     *
+     * @return {Boolean}
+     */
+    Microbe.removeStyle = function( selector, properties, media )
+    {
+        if ( !media && typeof properties === 'string' && properties !== 'all' )
+        {
+            media = properties;
+            properties = null;
+        }
+
+        media = media || 'none';
+
+        var _removeStyle = function( _el, _media )
+        {
+            _el.parentNode.removeChild( _el );
+            delete Microbe.__customCSSRules[ selector ][ _media ];
+        };
+
+        var style = Microbe.__customCSSRules[ selector ];
+
+        if ( style )
+        {
+            if ( properties === 'all' )
+            {
+                for ( var _mq in style )
+                {
+                    _removeStyle( style[ _mq ].el, _mq );
+                }
+            }
+            else
+            {
+                style = style[ media ];
+
+                if ( style )
+                {
+                    if ( Microbe.isArray( properties ) && !Microbe.isEmpty( properties ) )
+                    {
+                        for ( var i = 0, lenI = properties.length; i < lenI; i++ )
+                        {
+                            if ( style.obj[ properties[ i ] ] )
+                            {
+                                delete style.obj[ properties[ i ] ];
+                            }
+                        }
+                        if ( Microbe.isEmpty( style.obj ) )
+                        {
+                            _removeStyle( style.el, media );
+                        }
+                        else
+                        {
+                            Microbe.insertStyle( selector, style.obj, media );
+                        }
+                    }
+                    else
+                    {
+                        _removeStyle( style.el, media );
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+        else
+        {
+            return false;
+        }
+
+        return true;
+    };
+
+
+    /**
      * nothing happens
      *
      * https://en.wikipedia.org/wiki/Xyzzy_(computing)
@@ -4443,6 +4602,30 @@ module.exports = function( Microbe )
      * @return {void}
      */
     Microbe.noop    = function() {};
+
+
+    /**
+     * returns a function that can only be run once
+     *
+     * @param {Function}      _func                         function to run once
+     *
+     * @return {Function}
+     */
+    Microbe.once = function( _func, context )
+    {
+        var result;
+
+        return function()
+        {
+            if( _func )
+            {
+                result  = _func.apply( context || this, arguments );
+                _func   = null;
+            }
+
+            return result;
+        };
+    };
 
 
     /**
