663f219a9ccefd8ff236e3ed1d85a02bd6c79d00
Mouse Braun <mouse@knoblau.ch>
2015-11-28 16:39:50 +0100

    travis

--- tests/nightmare.js
+++ tests/nightmare.js
@@ -4,40 +4,40 @@ var vo          = require( 'vo' );
 var connect     = require( 'connect' );
 var serveStatic = require( 'serve-static' );
 
-var server = connect().use( serveStatic( process.cwd() ) ).listen( 8080 );
-console.log( server );
+var server = connect().use( serveStatic( process.cwd() ) ).listen( 8666 );
+
 var errors;
 
-// vo( run )( function( err, result )
-// {
-//     if ( err )
-//     {
-//         throw err;
-//     }
-//     else if ( errors !== 0 )
-//     {
-//         throw new Error( 'there were more than 0 errors (' + errors + ' to be exact)' );
-//     }
-//     else
-//     {
-//         console.log( 'All tests passed!' );
-//     }
-// } );
+vo( run )( function( err, result )
+{
+    if ( err )
+    {
+        throw err;
+    }
+    else if ( errors !== 0 )
+    {
+        throw new Error( 'there were more than 0 errors (' + errors + ' to be exact)' );
+    }
+    else
+    {
+        console.log( 'All tests passed!' );
+    }
+} );
 
 
-// function *run()
-// {
-//     var nightmare   = Nightmare();
+function *run()
+{
+    var nightmare   = Nightmare();
 
-//     errors = yield nightmare
-//         .goto( 'http://localhost:8080/tests/index.html' )
-//         .wait( '.pass' )
-//         .evaluate( function()
-//         {
-//             console.log( document.title );
-//             return document.getElementsByClassName( 'fail' ).length;
-//         } );
-//     server.close();
+    errors = yield nightmare
+        .goto( 'http://localhost:8666/tests/index.html' )
+        // .wait( '.pass' )
+        .evaluate( function()
+        {
+            console.log( document.title );
+            return document.getElementsByClassName( 'fail' ).length;
+        } );
+    server.close();
 
-//     yield nightmare.end();
-// }
+    yield nightmare.end();
+}
