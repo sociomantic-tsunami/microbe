41952cabb54474801e15955eb73a34b5780d57a9
Mouse Braun <mouse@knoblau.ch>
2015-11-28 13:42:17 +0100

    updated travis script for slack integration
    bumped tests version

--- tests/unit/selectorEngine/core.js
+++ tests/unit/selectorEngine/core.js
@@ -1,6 +1,6 @@
 /* global document, window, µ, $, QUnit, Benchmark, test  */
 var indexOf = Array.prototype.indexOf;
-var version = '0.4.12';
+var version = '0.4.13';
 
 module.exports = function( buildTest )
 {
